58fa585d40f14009fdb239632eb3df9f8b86f569
Jules Morel <jules.morel@ifpindia.org>
2022-09-01 12:20:06 +0200

    script aRchi

---
+++ reconstruction.R
@@ -0,0 +1,29 @@
+library("aRchi")
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)!=2) {
+  stop("Two arguments must be supplied (input directory and output directory).n", call.=FALSE)
+} else {
+   inDir=args[1];
+   outDir=args[2];
+}
+
+listInputFiles = list.files(inDir)
+
+for (file in listInputFiles) {
+  input = paste(inDir, fsep = .Platform$file.sep, file, sep = "")
+  outputfile=gsub(file, pattern=".xyz", replacement=".obj")
+  output = paste(outDir, fsep = .Platform$file.sep, outputfile, sep = "")
+  points = read.table(input)
+  ar = aRchi::build_aRchi()
+  ar = aRchi::add_pointcloud(ar,point_cloud = points)
+  ar = skeletonize_pc(ar)
+  ar = smooth_skeleton(ar)
+  ar =add_radius(ar)
+  
+  mesh=QSM2mesh(ar@QSM, tmesh = TRUE, sides = 16)
+  rgl::shade3d(mesh)
+  rgl::writeOBJ(output)
+  rgl::close3d()
+}
