1756c3181ced958cb7bfc026ecd1059f8c3638ec
Jules Morel <jules.morel@ifpindia.org>
2022-10-15 14:42:04 +0530

    changed requirements for files and dirs

--- reconstruction.py
+++ reconstruction.py
@@ -1,7 +1,6 @@
 import logging
 import numpy as np
 import subprocess
-
 import click
 
 logging.basicConfig(
@@ -15,9 +14,9 @@ logging.basicConfig(
 logger = logging.getLogger('slim')
 
 @click.command()
-@click.option('--segmentation_result_file', required=True, type=click.Path(exists=True), help='the ASCII file resulting from the deep learning model segmentation.')
-@click.option('--output_directory_ascii', required=True, type=click.Path(exists=True), help='this directory stores the point clouds of the clusters.')
-@click.option('--output_directory_obj', required=True, type=click.Path(exists=True), help='this directory stores the tree models in the OBJ format.')
+@click.option('--segmentation_result_file', required=True, type=click.Path(exists=True, file_okay=True, dir_okay=False), help='the ASCII file resulting from the deep learning model segmentation.')
+@click.option('--output_directory_ascii', required=True, type=click.Path(exists=True, file_okay=False, dir_okay=True), help='this directory stores the point clouds of the clusters.')
+@click.option('--output_directory_obj', required=True, type=click.Path(exists=True, file_okay=False, dir_okay=True), help='this directory stores the tree models in the OBJ format.')
 @click.option('--smooth_tol', type=float, default=45., help='If the deviation between points normals is less than the smoothness tolerance (in degrees) then they are suggested to be in the same cluster.')
 @click.option('--curv_th', type=float, default=10., help='If this curvature at the current point is less than the curvature threshold then the algorithm will continue the growth of the cluster using the newly added point.')
 @click.option('--cluster_size_min', type=int, default=1000, help='minimum number of points to form a cluster  ')
@@ -28,7 +27,7 @@ logger = logging.getLogger('slim')
 @click.option('--archi_D', type=float, default=0.3, help='the distance of research for point neighborhood')
 @click.option('--archi_cl_dist', type=float, default=0.1, help='the clustering distance ')
 @click.option('--archi_max_d', type=float, default=0.05, help='the maximum searching distance for skeleton building ')
-def reconstruction_with_aRchi(segmentation_result_file, output_directory_ascii, output_directory_obj, smooth_tol=45., curv_th=10., cluster_size_min=1000, cluster_size_max=1000000000, minsize_cluster_Z=2., mean_k=8, sd_mul_dev=0.5, archi_D=0.3, archi_cl_dist=0.1, archi_max_d=0.05):
+def reconstruction_with_aRchi(segmentation_result_file, output_directory_ascii, output_directory_obj, smooth_tol, curv_th, cluster_size_min, cluster_size_max, minsize_cluster_Z, mean_k, sd_mul_dev, archi_D, archi_cl_dist, archi_max_d):
     """Assign class=2 to the ground points, class=3 to the wood points and class=1 to the remaining point in the .laz files provided"""
 
     #Clustering
